c2e4fda31c7335a52f88de02be2cf5f33a786e84
Joseph Gabito <codehaiku.io@gmail.com>
2017-03-08 15:14:23 +0800

    up file-upload

--- transactions/file-upload.php
+++ transactions/file-upload.php
@@ -1,8 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 require_once SHAREDRIVE_DIR_PATH . 'vendor/autoload.php';
 
 use Symfony\Component\Filesystem\Filesystem;
@@ -15,13 +11,39 @@ $upload_dir = wp_upload_dir();
 $upload_dir = trailingslashit( $upload_dir['basedir'] );
 $sharedrive_upload_dir = trailingslashit( $upload_dir . 'sharedrive' );
 $user_upload_dir = $sharedrive_upload_dir . trailingslashit( absint( $_POST['post_id'] ) ) . $user_id;
+$max_upload_size = wp_max_upload_size();
+$tmp_file_size = 0;
+
+if ( isset( $_FILES['file']['size'] ) ) {
+	$tmp_file_size = $_FILES['file']['size'];
+}
+
+// Basic Validations.
+
+// File size should not be 0. File size is usually empty when uploaded file > max_upload_size.
+if ( 0 === $tmp_file_size  ) {
+	$response = wp_json_encode(array(
+			'status' => 201,
+			'message' => sprintf( 
+				__('Max upload size issue. Uploaded files should be less than %s', 'sharedrive'), 
+				Sharedrive\Helpers::formatSize( $max_upload_size ) 
+				)
+		));
+	echo $response;
+	Sharedrive\Helpers::stop();
+}
 
 // Cleaning up.
 if ( $sensio_fs->exists( $user_upload_dir ) ) {
 	try {
 		$sensio_fs->remove( $user_upload_dir );
 	} catch ( IOExceptionInterface $e ) {
-		echo "An error occurred cleaing up directory " . $e->getMessage();
+		$response = wp_json_encode(array(
+				'status' => 201,
+				'message' => __('An error occurred cleaing up directory ', 'sharedrive') . $e->getMessage()
+			));
+		echo $response;
+		Sharedrive\Helpers::stop();
 	}
 }
 // Check if there is already a directory created for the user.
@@ -31,13 +53,18 @@ if ( ! $sensio_fs->exists( $user_upload_dir ) ) {
 	    $sensio_fs->mkdir( $user_upload_dir, 0755 );
 	    // Upload (copy) the submitted file into the directory.
 	  	sd_upload_file( $_FILES, $user_upload_dir, $sensio_fs );
+
 	} catch ( IOExceptionInterface $e ) {
 		// Throw an error if there is was an error create a file.
-	    echo "An error occurred while creating your directory at " . $e->getMessage();
+	    $response = wp_json_encode(array(
+				'status' => 201,
+				'message' => __('An error occurred while creating your directory at ', 'sharedrive') . $e->getMessage()
+			));
+		echo $response;
+		Sharedrive\Helpers::stop();
 	}
 } else {
 	// The directory already exists. Time to copy the temporary sent file to that directory.
-	
 	sd_upload_file( $_FILES, $user_upload_dir, $sensio_fs );
 }
 
@@ -55,14 +82,28 @@ function sd_upload_file( $files = '', $destination = '', Filesystem $sensio_fs )
 
 		// Move from temporary to static directory.
 		$sensio_fs->copy( $tmp_file_location, trailingslashit( $destination ) . $destination_file_name );
+			
 		// Update the post meta.
 		update_post_meta( absint( $_POST['post_id'] ), 'sharedrive_file_name', $destination_file_name );
 		update_post_meta( absint( $_POST['post_id'] ), 'sharedrive_file_type', $tmp_file_type );
 		update_post_meta( absint( $_POST['post_id'] ), 'sharedrive_file_size', $tmp_file_size );
 
+		$response = wp_json_encode(array(
+				'status' => 200,
+				'message' => __('File has been successfully uploaded. Click the "Update" button in the right side of the corner to update the post meta display.', 'sharedrive')
+			));
+		echo $response;
+		Sharedrive\Helpers::stop();
+		
+		
 	} catch( IOExceptionInterface $e ) {
-		echo  $e->getMessage();
+		$response = wp_json_encode(array(
+				'status' => 201,
+				'message' => __('An error occurred while moving your uploaded file. Make sure your uploads folder is writable.', 'sharedrive')
+			));
+		echo $response;
+		Sharedrive\Helpers::stop();
 	}
 }
-echo 'RESPONSE';
-die();
+
+Sharedrive\Helpers::stop();
